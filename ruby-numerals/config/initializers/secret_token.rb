494aa4ec6a4934bfc2a0fcd5dbaa622d66e07fb9
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-09-20 21:25:36 +0530

    Added rails and rspec

---
+++ ruby-numerals/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyNumerals::Application.config.secret_token = '3f286833bddcb005b4ccb3296cc28a897e7e25a1997fbf5e76dcf869422ea789cfeb6678ddac99a8ecbd56e75b7233b3cc961cc282af766f363a61c50eaab699'
